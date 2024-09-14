3f4d64f485147f767fc4ac5f4362175404dcae8a
JuanIkarigit config --global user.name JuanIkari <j.arcila1@utp.edu.co>
2024-09-14 17:43:06 -0500

    paso 4 (Nav-Tab) ✅

--- Nav-Tab/Screens/QuizScreen.js
+++ Nav-Tab/Screens/QuizScreen.js
@@ -1,12 +1,71 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 
-// Esta pantalla puede recibir el objeto 'navigation', aunque no es necesario para este ejemplo
-export default function SettingsScreen() {
+const questions = [
+  {
+    id: "1",
+    question: "¿Cuál es el color del cielo?",
+    options: ["Rojo", "Verde", "Azul", "Amarillo"],
+  },
+  {
+    id: "2",
+    question: "¿Qué es React Native?",
+    options: [
+      "Un marco de trabajo para aplicaciones web",
+      "Una biblioteca para construir interfaces de usuario",
+      "Un sistema de gestión de bases de datos",
+      "Una herramienta de diseño gráfico",
+    ],
+  },
+  {
+    id: "3",
+    question: "¿Cuál es la capital de Francia?",
+    options: ["Londres", "Berlín", "Madrid", "París"],
+  },
+];
+
+export default function QuizScreen() {
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+
+  const AnswerSelect = (questionId, answer) => {
+    setSelectedAnswers((prev) => ({
+      ...prev,
+      [questionId]: answer,
+    }));
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      {/* Contenido simple de la pantalla Settings */}
-      <Text>QuizScreen</Text>
+    <View style={styles.container}>
+      {questions.map((question) => (
+        <View key={question.id} style={styles.questionContainer}>
+          <Text style={styles.question}>{question.question}</Text>
+          {question.options.map((option, index) => (
+            <Button
+              key={index}
+              title={option}
+              onPress={() => AnswerSelect(question.id, option)}
+              color={selectedAnswers[question.id] === option ? "blue" : "gray"}
+            />
+          ))}
+        </View>
+      ))}
+      <Button title="Enviar Respuestas" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  questionContainer: {
+    marginBottom: 20,
+  },
+  question: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+});
