ed42153f99e0be56f2fd94120322b3f06beacee6
JuanIkarigit config --global user.name JuanIkari <j.arcila1@utp.edu.co>
2024-09-15 10:48:38 -0500

    paso 5 ✅

--- Nav-Tab/App.js
+++ Nav-Tab/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import QuizScreen from "./Screens/QuizScreen";
 import OptionScreen from "./Screens/OptionScreen";
@@ -11,7 +12,23 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Image") {
+              iconName = focused ? "image" : "image-outline";
+            } else if (route.name === "Options") {
+              iconName = focused ? "options" : "options-outline";
+            } else if (route.name === "Quiz") {
+              iconName = focused ? "clipboard" : "clipboard-outline";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
         <Tab.Screen name="Image" component={ImageScreen} />
         <Tab.Screen name="Options" component={OptionScreen} />
         <Tab.Screen name="Quiz" component={QuizScreen} />
